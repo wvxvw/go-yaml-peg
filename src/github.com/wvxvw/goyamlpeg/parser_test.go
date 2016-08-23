55e8aec8731c94df09d6f6ab9dd6c6e0d1ae03f6
wvxvw <olegsivokon@gmail.com>
2016-08-23 00:20:00 +0000

    Corrected tag parsing. Added URI parsing (wrong).

--- src/github.com/wvxvw/goyamlpeg/parser_test.go
+++ src/github.com/wvxvw/goyamlpeg/parser_test.go
@@ -14,7 +14,6 @@ body
 		t.Fatalf("Parsing failed: %+v", err)
 	}
 	yaml.Execute()
-	// yaml.PrintSyntax()
 	yaml.PrintSyntaxTree()
 }
 
@@ -35,4 +34,61 @@ morebody
 	if err := yaml.Parse(); err != nil {
 		t.Fatalf("Parsing failed: %+v", err)
 	}
+}
+
+func TestQuotedLiteral(t *testing.T) {
+	yaml := &Yaml{
+		Buffer: `
+"simple quote"
+`,
+	}
+	yaml.Init()
+	if err := yaml.Parse(); err != nil {
+		t.Fatalf("Parsing failed: %+v", err)
+	}
+	yaml.PrintSyntaxTree()
+}
+
+func TestAposLiteral(t *testing.T) {
+	yaml := &Yaml{
+		Buffer: `
+'simple apostrophe'
+`,
+	}
+	yaml.Init()
+	if err := yaml.Parse(); err != nil {
+		t.Fatalf("Parsing failed: %+v", err)
+	}
+	yaml.PrintSyntaxTree()
+}
+
+func TestQuotedCodepoints(t *testing.T) {
+	yaml := &Yaml{
+		Buffer: `
+"simple \x36 quote \u123f"
+`,
+	}
+	yaml.Init()
+	if err := yaml.Parse(); err != nil {
+		t.Fatalf("Parsing failed: %+v", err)
+	}
+	yaml.PrintSyntaxTree()
+}
+
+func TestQuotedMultiline(t *testing.T) {
+	yaml := &Yaml{
+		Buffer: `
+"
+first line
+second line
+
+third line
+"
+`,
+	}
+	yaml.Init()
+	if err := yaml.Parse(); err != nil {
+		t.Fatalf("Parsing failed: %+v", err)
+	}
+	yaml.PrintSyntaxTree()
 }
